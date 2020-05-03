5016d5bd328937027be25ba7eecbb0f793d5202f
javierc22 <javier.ec.22@gmail.com>
2020-05-03 13:31:23 -0400

    Agregar 'sessions controller'

---
+++ app/controllers/api/v1/user/sessions_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::User::SessionsController < ApplicationController
+  def sign_in
+  end
+
+  def sign_out
+  end
+
+  def me
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      namespace :user do
+        post 'sign_in', to: 'sessions#sign_in'
+        delete 'sign_out', to: 'sessions#sign_out'
+        get 'me', to: 'sessions#me'
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
